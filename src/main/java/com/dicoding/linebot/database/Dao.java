f8d38e6f1d938ec208152288394d73975fd7f8a9
Angela Marpaung <angelamarpaung99@gmail.com>
2020-03-17 23:43:12 +0700

    dicoding event bot

---
+++ src/main/java/com/dicoding/linebot/database/Dao.java
@@ -0,0 +1,17 @@
+package com.dicoding.linebot.database;
+
+import com.dicoding.linebot.model.JointEvents;
+import com.dicoding.linebot.model.User;
+
+import java.util.List;
+
+public interface Dao
+{
+    public List<User> get();
+    public List<User> getByUserId(String aUserId);
+    public int registerLineId(String aUserId, String aLineId, String aDisplayName);
+    public int joinEvent(String aEventId, String aUserId, String aLineId, String aDisplayName);
+    public List<JointEvents> getEvent();
+    public List<JointEvents> getByEventId(String aEventId);
+    public List<JointEvents> getByJoin(String aEventId, String aUserId);
+}
